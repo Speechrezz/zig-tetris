a868d69442c9418e194014a38794e30402aab066
Speechrezz <thespeechlessmusic@gmail.com>
2026-04-30 11:18:36 -0700

    Fixed board margins

--- src/Board.zig
+++ src/Board.zig
@@ -14,8 +14,8 @@ pub const block_size = block.size;
 pub const block_count = board_width * board_height;
 
 const outline_width = block.outline_width;
-const pixel_width = board_width * block_size + 2 * outline_width;
-const pixel_height = board_height * block_size + 2 * outline_width;
+const pixel_width = board_width * block_size;
+const pixel_height = board_height * block_size;
 
 pos: Position,
 blocks: [block_count]TetrominoKind = [_]TetrominoKind{.nil} ** block_count,
